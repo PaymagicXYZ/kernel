317a42e837f1ed947bda4e5f1d6cdfdcb754a365
Rory Hellyer <rory@webmodularity.com>
2024-02-23 13:20:01 -0800

    fix salt

--- scripts/DeployMultiECDSAFactoryPatchProdPartial.s.sol
+++ scripts/DeployMultiECDSAFactoryPatchProdPartial.s.sol
@@ -7,10 +7,10 @@ import "forge-std/Script.sol";
 import "forge-std/console.sol";
 
 contract DeployMultiECDSAFactoryPatchProd is Script {
-    function run() public {
+    function run(bytes32 salt) public {
         uint256 key = vm.envUint("DEPLOYER_PRIVATE_KEY");
         vm.startBroadcast(key);
-        bytes32 salt = bytes32(uint256(0x2));
+
         Kernel kernelImplementation = new Kernel{salt: salt}(
             IEntryPoint(0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789)
         );
